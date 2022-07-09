dd9c2d18e7f6f4868a9ed4996bd9c0091df71da1
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-07-09 09:28:59 +0800

    add routing and import class

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return response("<h1>Hello world</h1>", 200)
+    ->header("Content-Type", "text/plain")
+    ->header("asdf", "test header");
+});
+
+Route::get('/posts/{id}', function($id){
+    dd($id);
+    return response("Post " . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request) {
+    // dd($request);
+    return $request->name . ' ' . $request-> city;
+});
+// http://jobhunt.test/search?name=Jhon&city=Urdaneta
