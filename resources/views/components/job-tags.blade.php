29194d9512561a1dd1f3be4a21701376bfb29ccb
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-07-20 12:00:02 +0800

    create component for tags

---
+++ resources/views/components/job-tags.blade.php
@@ -0,0 +1,22 @@
+<ul class="flex">
+  <li
+      class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+  >
+      <a href="#">Laravel</a>
+  </li>
+  <li
+      class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+  >
+      <a href="#">API</a>
+  </li>
+  <li
+      class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+  >
+      <a href="#">Backend</a>
+  </li>
+  <li
+      class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+  >
+      <a href="#">React</a>
+  </li>
+</ul>
