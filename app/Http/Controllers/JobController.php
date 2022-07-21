516b616c2d8c2f64004f1078a57b4ed4aedf6965
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-07-21 08:47:44 +0800

    update jobs blade

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -9,14 +9,14 @@ class JobController extends Controller
 {
     // Show all job listings
     public function index() {
-        return view('jobs', [
+        return view('jobs.index', [
             'jobs' => Job::all()
         ]);
     }
 
     // Show single job listing
     public function show(Job $job){
-        return view('job', [
+        return view('jobs.show', [
             'job' => $job
         ]);
     }
