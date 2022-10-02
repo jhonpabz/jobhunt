8d6c1c909f3b1967e3ade2cf86352d2d22953bf7
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-10-02 21:37:40 +0800

    remove edit and delete in job view page

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -50,7 +50,7 @@
                         </div>
                     </div>
                 </x-card>
-                <x-card class="mt-4 p-2 flex space-x-6">
+                {{-- <x-card class="mt-4 p-2 flex space-x-6">
                     <a href="/jobs/{{$job->id}}/edit">
                         <i class="fa-solid fa-pencil"></i> Edit
                     </a>
@@ -60,7 +60,7 @@
                         @method('DELETE')
                         <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
                     </form>
-                </x-card>
+                </x-card> --}}
             </div>
 
 </x-layout>
